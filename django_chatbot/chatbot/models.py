08ef114283d703d33f8c9f2523edb596049469ff
Aman Singh Samant <56504833+Aman-Singh-Samant@users.noreply.github.com>
2023-08-18 22:38:53 +0530

    Add files via upload
    first commit

---
+++ django_chatbot/chatbot/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.auth.models import AnonymousUser
+
+# Create your models here.
+
+class Chat(models.Model):
+    
+    user= models.ForeignKey(User, on_delete=models.CASCADE)
+    message=models.TextField()
+    response=models.TextField()
+    created_at=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        
+        return f'{self.user.username}: {self.message}'
+
+class AnonymousUser():
+    id=0
